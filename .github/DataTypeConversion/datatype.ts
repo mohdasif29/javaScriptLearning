cc11c3f2813feebfe69abefa154b4c80f3c2c3eb
mohdasif29 <67165414+mohdasif29@users.noreply.github.com>
2024-03-30 11:39:36 +0000

    type conversion to Number and String.

---
+++ .github/DataTypeConversion/datatype.ts
@@ -0,0 +1,27 @@
+let score = undefined
+
+let numberScore = Number(score);
+// console.log(typeof numberScore);
+// console.log(numberScore);
+
+/*  score => output
+    "33" => 33
+    "asif" => NaN
+    true => 1 , false => 0
+    undefined => undefined
+    null => 0
+*/
+let age = true;
+
+let convertToString = String(age);
+console.log("type",typeof convertToString);
+console.log("value",convertToString);
+
+
+/* variable => type , value
+    33 => string , 33
+    true => string , true
+    null => string , null
+    undefined => string , undefined
+*/
+
